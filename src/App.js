d21f1115c70ef394c96e24f950e158dee53902d7
lexjames06 <ajs.stewart@btinternet.com>
2020-03-03 13:53:22 +0000

    Initialise app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [player, setPlayer] = useState('');
+  const [playerSearching, setPlayerSearching] = useState('');
+  const [playerSearched, setPlayerSearched] = useState('');
+  const [searchStatus, setSearchStatus] = useState(false)
+
+  document.title = 'NBA Draft Details'
+
+  function updateSearchedPlayer(e) {
+    setPlayerSearching(e.target.value);
+  }
+
+  function loading() {
+    setSearchStatus('...loading');
+    setPlayerSearched(playerSearching);
+  }
+
+  useEffect(() => {
+    async function findPlayer() {
+      const response = await fetch('http://data.nba.net/10s/prod/v1/2019/players.json');
+      const data = await response.json();
+      const nbaPlayer = data.league.standard
+        .find(player => {
+          let playerName = `${player.firstName} ${player.lastName}`;
+          return playerName.toLowerCase() === playerSearched.toLowerCase();
+        });
+      if(nbaPlayer) {
+        setPlayer(nbaPlayer);
+        setSearchStatus(false);
+        document.title = `${player.firstName} ${player.lastName} Draft Details`;
+      } else if (playerSearched) {
+        setSearchStatus('No player found');
+      } else {
+        setSearchStatus(false);
+      }
+    }
+    searchStatus && findPlayer();
+  });
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h3>Search NBA Player Draft Details</h3>
+      <div>
+        <input type='text' onChange={e => updateSearchedPlayer(e)}/>
+        <button type='submit' onClick={() => loading()}>Find</button>
+      </div>
+      {
+        searchStatus ? <div>{searchStatus}</div> : player &&
+            (
+              <div>
+                <h4>{`${player.firstName} ${player.lastName}`}</h4>
+                <div>Draft year: {player.draft.seasonYear || player.teams[0].seasonStart}</div>
+                <div>Draft Position: {player.draft.pickNum || 'Undrafted'}</div>
+              </div>
+            )
+      }
     </div>
   );
 }
